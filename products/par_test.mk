543a6391c215a0f9d2bbc66007dc43cab32e8793
rudij7 <rudij7@users.noreply.github.com>
2014-09-29 15:42:41 +0530

    Create par_test.mk

---
+++ products/par_test.mk
@@ -0,0 +1,40 @@
+# Copyright (C) 2014 XPerience
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+
+ifeq (par_test,$(TARGET_PRODUCT))
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_xhdpi
+
+# Build paprefs from sources
+PREFS_FROM_SOURCE ?= false
+
+# Inherit telephony common stuff
+$(call inherit-product, vendor/PAradox/configs/telephony.mk)
+
+# Include ParanoidAndroid common configuration
+include vendor/PAradox/main.mk
+
+# Override AOSP build properties
+# Release name
+PRODUCT_RELEASE_NAME := Test Device
+PRODUCT_NAME := par_test
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/test/test/full_test.mk)
+
+endif
+ 
