2be6bd3bc1a051747b8d84329d0410555d08bbde
rudij7 <rudij7@users.noreply.github.com>
2014-09-29 15:11:57 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 add_lunch_combo par_grouper-userdebug
 add_lunch_combo par_falcon-userdebug
 add_lunch_combo par_tilapia-userdebug
+add_lunch_combo par_ghost-userdebug
 add_lunch_combo par_testdevice-userdebug
